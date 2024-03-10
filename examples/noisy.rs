bb36eb8a94b57c6b663eed88e76acfc67eaa0f00
Matthew Tran <0e4ef622@gmail.com>
2024-03-10 18:24:27 -0500

    examples

---
+++ examples/noisy.rs
@@ -0,0 +1,43 @@
+use std::time::Instant;
+
+use nalgebra::{Vector2, Point3};
+use sqpnp::types::SolverParameters;
+
+fn main() {
+    let pts3 = [
+        Point3::new(-0.429857595273321, -0.441798127281825, 0.714342354521372),
+        Point3::new(-2.1568268264648, 0.113521604867983, -0.148634122716948),
+        Point3::new(0.694636908485644, -0.737067927134015, -1.38877746946909),
+        Point3::new(-1.07051455287146, -1.2122304801284, -0.841002964233812),
+        Point3::new(0.509844073252947, -1.07097319594739, 0.675410167109412),
+        Point3::new(0.40951585099, 2.2300713816052, 0.365229861025625),
+        Point3::new(2.04320214188098, 1.11847674401846, 0.623432173763436),
+    ];
+
+    let pts2 = [
+        // noisy
+        Vector2::new(0.138854772853285, -0.00157437083896972),
+        Vector2::new(0.149353089173631, 0.269826809256435),
+        Vector2::new(-0.118391028248405, -0.0842834292914752),
+        Vector2::new(0.0937833539430025, 0.0473371294380393),
+        Vector2::new(0.101410594775151, -0.179030803711188),
+        Vector2::new(-0.294749181228375, 0.221134043355639),
+        Vector2::new(-0.334084299358372, -0.21071853326318),
+    ];
+    let start = Instant::now();
+    let solver = sqpnp::PnpSolver::new(&pts3, &pts2, vec![], SolverParameters::default());
+
+    let mut stop = Instant::now();
+    if let Some(mut solver) = solver {
+        solver.Solve();
+        stop = Instant::now();
+        println!("SQPnP found {} solution(s)", solver.NumberOfSolutions());
+        for i in 0..solver.NumberOfSolutions() {
+            println!("Solution {i}");
+            solver.SolutionPtr(i).unwrap().print();
+            println!(" Average squared projection error : {:e}", solver.AverageSquaredProjectionErrors()[i]);
+        }
+    }
+    let duration = stop - start;
+    println!("Time taken by SQPnP: {} microseconds", duration.as_micros());
+}
---
+++ examples/plain.rs
@@ -0,0 +1,43 @@
+use std::time::Instant;
+
+use nalgebra::{Vector2, Point3};
+use sqpnp::types::SolverParameters;
+
+fn main() {
+    let pts3 = [
+        Point3::new(-0.429857595273321, -0.441798127281825, 0.714342354521372),
+        Point3::new(-2.1568268264648, 0.113521604867983, -0.148634122716948),
+        Point3::new(0.694636908485644, -0.737067927134015, -1.38877746946909),
+        Point3::new(-1.07051455287146, -1.2122304801284, -0.841002964233812),
+        Point3::new(0.509844073252947, -1.07097319594739, 0.675410167109412),
+        Point3::new(0.40951585099, 2.2300713816052, 0.365229861025625),
+        Point3::new(2.04320214188098, 1.11847674401846, 0.623432173763436),
+    ];
+
+    let pts2 = [
+        // no noise
+        Vector2::new(0.139024436737141, -0.00108631784422283),
+        Vector2::new(0.149897105048989, 0.270584578309815),
+        Vector2::new(-0.118448642309468, -0.0844116551810971),
+        Vector2::new(0.0917181969674735, 0.0435196877212059),
+        Vector2::new(0.100243308685939, -0.178506520365217),
+        Vector2::new(-0.296312157121094, 0.220675975198136),
+        Vector2::new(-0.331509880499455, -0.213091587841007),
+    ];
+    let start = Instant::now();
+    let solver = sqpnp::PnpSolver::new(&pts3, &pts2, vec![], SolverParameters::default());
+
+    let mut stop = Instant::now();
+    if let Some(mut solver) = solver {
+        solver.Solve();
+        stop = Instant::now();
+        println!("SQPnP found {} solution(s)", solver.NumberOfSolutions());
+        for i in 0..solver.NumberOfSolutions() {
+            println!("Solution {i}");
+            solver.SolutionPtr(i).unwrap().print();
+            println!(" Average squared projection error : {:e}", solver.AverageSquaredProjectionErrors()[i]);
+        }
+    }
+    let duration = stop - start;
+    println!("Time taken by SQPnP: {} microseconds", duration.as_micros());
+}
